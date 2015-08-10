7e255430c7e06dda5a1ddd11785defc729900d68
Sukh <Sukh@Sukhi>
2015-08-10 01:23:53 -0400

    asd

---
+++ mate1/src/selenium/Details.java
@@ -0,0 +1,8 @@
+package selenium;
+
+public class Details {
+	String Title,LookingFor,Agedbetween,BirthDate, Country, PostalCode ,Ethnicity ,BodyType ,Height,
+	HairColor, EyeColor ,Realtionship, HaveChildren, Education ,AnnualIncome ;
+	
+
+}
